60e7f4fbf2fc995a52028f2e9fe8724b5308722b
Maik Jablonski <jablonski@neoskop.de>
2024-01-02 14:48:50 +0100

    initial commit

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { downloadTemplate } from "giget";
+import path from "node:path";
+
+const { source, dir } = await downloadTemplate("github:jeasx/quickstart", {
+  install: true,
+  dir: process.argv[2],
+});
+
+const target = path.relative(process.cwd(), dir);
+
+console.info(
+  `\n✅ Extracted \x1b[35m${source}\x1b[0m into \x1b[35m${target}\x1b[0m.`,
+  `\n🚀 To start development: \x1b[33mcd ${target} && npm run dev\x1b[0m`
+);

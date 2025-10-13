c9e7d42012fec43beb66951c2d97e0ae36b98dbf
Maik Jablonski <jablonski@neoskop.de>
2025-10-13 10:26:41 +0200

    fix: user friendly error message when project directory already exists

--- cli.js
+++ cli.js
@@ -13,28 +13,36 @@ await new Command()
   .option("--with-docker", "install with Docker support")
   .option("-f,--force", "force installation, overwrite target directory")
   .action(async (name, { force, withVercel, withDocker }) => {
-    const { source, dir } = await downloadTemplate("github:jeasx/quickstart", {
-      install: true,
-      force: force,
-      dir: name,
-    });
+    try {
+      const { source, dir } = await downloadTemplate(
+        "github:jeasx/quickstart",
+        {
+          install: true,
+          force: force,
+          dir: name,
+        }
+      );
 
-    const target = path.relative(process.cwd(), dir);
+      const target = path.relative(process.cwd(), dir);
 
-    const resourcesToRemove = [];
-    if (withDocker === undefined) {
-      resourcesToRemove.push("Dockerfile", ".dockerignore");
+      const resourcesToRemove = [];
+      if (withDocker === undefined) {
+        resourcesToRemove.push("Dockerfile", ".dockerignore");
+      }
+      if (withVercel === undefined) {
+        resourcesToRemove.push("api", "vercel.json");
+      }
+      resourcesToRemove.forEach((pathname) =>
+        fs.rmSync(path.join(target, pathname), { recursive: true })
+      );
+
+      console.info(
+        `\n✅ Extracted \x1b[35m${source}\x1b[0m into \x1b[35m${target}\x1b[0m.`,
+        `\n⭐ Start development: \x1b[33mcd ${target} && npm run build && npm run dev\x1b[0m`
+      );
+    } catch (e) {
+      console.error(`❌ ${e}`);
+      process.exit(1);
     }
-    if (withVercel === undefined) {
-      resourcesToRemove.push("api", "vercel.json");
-    }
-    resourcesToRemove.forEach((pathname) =>
-      fs.rmSync(path.join(target, pathname), { recursive: true })
-    );
-
-    console.info(
-      `\n✅ Extracted \x1b[35m${source}\x1b[0m into \x1b[35m${target}\x1b[0m.`,
-      `\n⭐ Start development: \x1b[33mcd ${target} && npm run build && npm run dev\x1b[0m`
-    );
   })
   .parseAsync();
